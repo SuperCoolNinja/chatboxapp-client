25efd8eb119b9fb81812f5a0aaf5b76e354a4e67
SuperCoolNinja <sbpro570@gmail.com>
2023-10-31 19:35:00 +0100

    init

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import socketIOClient from "socket.io-client";
 
-const IS_PROD = true;
+const IS_PROD = false;
 
 const ENDPOINT = IS_PROD
-  ? "https://chatboxapp-server.vercel.app/"
+  ? "https://theoretical-dorian-supercoolninja.koyeb.app/"
   : "http://localhost:4000";
 export const socket = socketIOClient(ENDPOINT);
