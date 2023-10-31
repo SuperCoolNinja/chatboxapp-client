4ddb6d064c104db350db203f49348002fd61689c
SuperCoolNinja <sbpro570@gmail.com>
2023-10-31 19:35:42 +0100

    init

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const IS_PROD = false;
+const IS_PROD = true;
 
 const ENDPOINT = IS_PROD
   ? "https://theoretical-dorian-supercoolninja.koyeb.app/"
